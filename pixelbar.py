a905ebdb1d68c640a650d7df8a93a27fd572b1f0
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2025-02-18 03:39:39 +0800

    pixelbar 更新
    峰值速度可以轻松达到 200KB/s，但是 decode 报错，得到错误结果。
    目前只有 -Q 1 -B 10 能够解码

--- pixelbar.py
+++ pixelbar.py
@@ -1,3 +1,4 @@
+import struct
 import numpy as np
 from PIL import Image
 
@@ -5,6 +6,7 @@ class PixelBar:
     def __init__(self, version=40, box_size=1, border_size=1, pixel_bits=8):
         box = 21 + 4*version + 2*border_size
         width = height = box * box_size
+        self.version = version
         self.width = width
         self.height = height
         self.width_data_box = width//box_size - 2*border_size
@@ -14,7 +16,7 @@ class PixelBar:
 
         self.pixel_bits = pixel_bits # 8 or 16
         self.mode = 1 if pixel_bits == 8 else 2
-        self.max_data_size = self.width_data_box * self.height_data_box * self.mode
+        self.max_data_size = self.width_data_box * self.height_data_box * self.mode - 4
 
     def _mode1_encode(self, data):
         """3-3-2 编码模式"""
@@ -25,9 +27,9 @@ class PixelBar:
             b = byte & 0x03          # 低2位
             
             # 使用每个通道的高位保存数据（不容易失真）
-            r = (r << 5)
-            g = (g << 5)
-            b = (b << 6)
+            r = (r << 5) | 0x10  # 在有效位后紧随 1 位设置为 1，防止像素向下偏移或者向上偏移，影响高位
+            g = (g << 5) | 0x10
+            b = (b << 6) | 0x20
             pixels.append((r, g, b))
         return pixels
 
@@ -49,23 +51,29 @@ class PixelBar:
             pixels.append((r, g, b))
         return pixels
 
-    def encode(self, data):
+    def encode(self, data_):
         # 数据长度校验
-        if len(data) > self.max_data_size:
-            raise ValueError(f"数据过大，最大支持 {self.max_data_size} 字节")
+        if len(data_) > self.max_data_size:
+            raise ValueError(f"pixelbar version {self.version} mode {self.mode}, max {self.max_data_size} bytes, get {len(data_)} bytes")
         
+        data = struct.pack('I', len(data_)) + data_
         # 选择编码模式
         if self.mode == 1:
             pixels = self._mode1_encode(data)
         elif self.mode == 2:
             pixels = self._mode2_encode(data)
         else:
-            raise ValueError("不支持的编码模式")
+            raise ValueError("unsupported pixelbar mode")
 
         # 生成图像矩阵
         B, b, w, h = self.box_size, self.border_size, self.width_data_box, self.height_data_box
         # arr = np.zeros((self.height+2*b, self.width+2*b, 3), dtype=np.uint8)
         arr = np.full(((h+2*b)*B, (w+2*b)*B, 3), 255, dtype=np.uint8)
+        arr[:B*b, :]    = (255, 255, 0)
+        arr[:, -B*b:]   = (255, 0, 0)
+        arr[-B*b:, :]   = (0, 255, 0)
+        arr[B*b:, :B*b] = (0, 0, 255)  # 跳过左上角
+        
         for p in range(len(pixels)):
             y, x = divmod(p, w)
             if y < h:
@@ -85,6 +93,7 @@ class PixelBar:
         arr = np.array(img)
         
         # detect box size
+        # TODO: 检测效果不行，需要改进
         def pixels_are_equal(pixel1, pixel2, rtol=10, atol=10):
             return np.allclose(pixel1, pixel2, rtol=rtol, atol=atol)
         def gcd(a, b):
@@ -108,15 +117,24 @@ class PixelBar:
                 box_size = gcd(sorted_count[2][0], gcd(sorted_count[0][0], sorted_count[1][0]))
             
         B = box_size
-        # 检查是否有效
-        # 检查边框 
-        if not np.all(arr[0:B, :] >=128):
+        b = border_size
+        # 检查是否有图像
+        # 通过检查边框是否全为白色来判断
+        # TODO: QR Code 的四个角的检测逻辑
+        p_a = np.mean(arr[:B*b, :]      , axis=(0, 1))
+        p_b = np.mean(arr[:, -B*b:]     , axis=(0, 1))
+        p_c = np.mean(arr[-B*b:, :]    , axis=(0, 1))
+        p_d = np.mean(arr[B*b:, :B*b]   , axis=(0, 1))
+        if not (p_a[0] > 200 and p_a[1] > 200 and p_a[2] < 50) or \
+           not (p_b[0] > 200 and p_b[1] < 50 and p_b[2] < 50) or \
+           not (p_c[0] < 50 and p_c[1] > 200 and p_c[2] < 50) or \
+           not (p_d[0] < 50 and p_d[1] < 50 and p_d[2] > 200):
             return None
         
-        b = border_size
         w, h = img.size
         w, h = w//B, h//B
         width_data_box, height_data_box = w - 2*b, h - 2*b
+        # print(f"box size: {B}, border size: {b}, data box size: {width_data_box}x{height_data_box}")
         pixels = []
         for y in range(height_data_box):
             for x in range(width_data_box):
@@ -129,6 +147,8 @@ class PixelBar:
             data = self._mode2_decode(pixels)
         else:
             raise ValueError("不支持的编码模式")
+        length = struct.unpack('I', bytes(data[:4]))[0]
+        data = data[4:4+length]
         return bytes(data)
     
     def _mode1_decode(self, pixels):
@@ -151,30 +171,42 @@ class PixelBar:
         return data
 
 if __name__ == "__main__":
-    pb = PixelBar(version=1, box_size=20, pixel_bits=8)
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Convert a file to a series of QR codes."
+    )
+    parser.add_argument(
+        "-E", "--encode", action="store_true", help="encode self to pixelbar.png"
+    )
+    parser.add_argument(
+        "-i", "--input", default='pixelbar.png', required=True, help="decode: image file"
+    )
+    parser.add_argument(
+        "-Q", "--qr-version", type=int, default=40, help="QRcode version"
+    )
+    parser.add_argument(
+        "-B", "--qr-box-size", type=float, default=3, help="QRcode pixels=(21+4*version+2(border))*box_size, When use screen output, can be float. "
+    )
+    parser.add_argument(
+        "-o", "--output", default="./pixelbar.txt", help="output file"
+    )
+    args = parser.parse_args()
     
-    with open('pixelbar.py', 'rb') as f:
-        test_data = f.read()
-    test_data = test_data[:pb.max_data_size]
+    pb = PixelBar(version=args.qr_version, box_size=int(args.qr_box_size), pixel_bits=8)
     
-    img = pb.encode(test_data)
-    img.show()
-    img.save('pixelbar.png')
+    if args.encode:
+        with open('pixelbar.py', 'rb') as f:
+            test_data = f.read()
+        test_data = test_data[:pb.max_data_size]
     
-    data = pb.decode(img)
-    print(data == test_data)
-    print(f"test data size {len(test_data)}: {test_data[:20]}")
-    print(f"decoded data size {len(data)}: {data[:20]}")
+        img = pb.encode(test_data)
+        img.show()
+        img.save('pixelbar.png')
+        exit(0)
     
-    import struct
-    img = Image.open('first.png')
-    print(img.size)
-    raw_data = pb.decode(img, box_size=20)
-    print(f"raw data size: {len(raw_data)}")
-    idx, num_chunks = struct.unpack('II', raw_data[:8])
-    data = raw_data[8:]
-    print(idx, num_chunks)
-    print(data[:20])
-    
-    img = pb.encode(raw_data)
-    img.show()
+    img = Image.open(args.input)
+    data = pb.decode(img, box_size=int(args.qr_box_size))
+    print(f"decode data: size {len(data)}")
+    print(f"{data[:20]}\n...\n{data[-20:]}")
+    with open(args.output, 'wb') as f:
+        f.write(data)
